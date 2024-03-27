cf53aed00ac384483a9edf02c223fb1ca0a22eab
Mica78 <mica78@yandex.ru>
2024-03-27 09:26:47 +0400

    fix rundom number

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import goblinWidjet from "./goblin.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const widjet = new goblinWidjet(document.querySelector(".widjet-container"));
-  let randomNumber = Math.floor(Math.random() * widjet.childrens.length);
+  let randomNumber = Math.floor(Math.random() * (widjet.childrens.length - 1));
   widjet.putGoblin(randomNumber);
   setInterval(() => {
     widjet.deleteGoblin();
@@ -10,7 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
       Math.random() * widjet.childrens.length
     );
     while (anotherRandomNumber === randomNumber) {
-      anotherRandomNumber = Math.floor(Math.random() * widjet.childrens.length);
+      anotherRandomNumber = Math.floor(
+        Math.random() * (widjet.childrens.length - 1)
+      );
     }
     widjet.putGoblin(anotherRandomNumber);
     randomNumber = anotherRandomNumber;
